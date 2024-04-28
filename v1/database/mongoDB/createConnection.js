479016b9d6942994e446ee7951dd358b955cdc95
JimmyJambino <47789013+jimmylosang@users.noreply.github.com>
2024-04-28 20:59:41 +0200

    remove deprecated option in mongoose.connect()

--- v1/database/mongoDB/createConnection.js
+++ v1/database/mongoDB/createConnection.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async (dbName) => {
     const uri = 'mongodb://127.0.0.1:27017/' + dbName;
     try {
-        await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(uri, { useNewUrlParser: true });
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
